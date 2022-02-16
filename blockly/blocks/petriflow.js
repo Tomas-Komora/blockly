418f9531e30d8eeec716aaf6e7fb5f2a24c528a9
tomaskomora <xkomorat@stuba.sk>
2022-02-16 13:07:25 +0100

    create all petriflow blocks

--- blockly/blocks/petriflow.js
+++ blockly/blocks/petriflow.js
@@ -21,3 +21,454 @@ Blockly.Blocks['block_assignrole'] = {
         this.setHelpUrl('https://petriflow.com/#/?id=actions');
     }
 };
+
+Blockly.Blocks['cancel_task_taskid'] = {
+    init: function() {
+        this.appendValueInput("taskId")
+            .setCheck("String")
+            .appendField("Cancel Task (taskId)");
+        this.setOutput(true);
+        this.setColour(65);
+        this.setTooltip('Cancel task by taskID');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['cancel_task_task_case'] = {
+    init: function() {
+        this.appendValueInput("taskId")
+            .appendField("Cancel Task (taskId)");
+        this.appendValueInput("aCase")
+            .appendField("Case");
+        this.setOutput(true);
+        this.setTooltip('Cancel task by taskId and Case');
+        this.setColour(65);
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['cancel_task_task'] = {
+    init: function() {
+        this.appendValueInput("taskId")
+            .appendField("Cancel Task (task)");
+        this.setOutput(true);
+        this.setTooltip('Cancel task by task');
+        this.setColour(65);
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['finish_task_taskid'] = {
+    init: function() {
+        this.appendValueInput("taskId")
+            .setCheck("String")
+            .appendField("Finish Task (task)");
+        this.setOutput(true);
+        this.setTooltip('Finish task by taskID');
+        this.setColour(65);
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+Blockly.Blocks['finish_task_task_case'] = {
+    init: function() {
+        this.appendValueInput("taskId")
+            .appendField("Finish Task (taskId)");
+        this.appendValueInput("aCase")
+            .appendField("Case");
+        this.setOutput(true);
+        this.setTooltip('Finish task by taskId and Case');
+        this.setColour(65);
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['finish_task_task'] = {
+    init: function() {
+        this.appendValueInput("taskId")
+            .appendField("Finish Task (task)");
+        this.setOutput(true);
+        this.setTooltip('Finish task by task');
+        this.setColour(65);
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['getdata_task'] = {
+    init: function() {
+        this.appendValueInput("task")
+            .setCheck("Task")
+            .appendField("getData(task)");
+        this.setOutput(true, "Map<String,Field>");
+        this.setTooltip('Get data by Task');
+        this.setColour(65);
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['getdata_transition'] = {
+    init: function() {
+        this.appendValueInput("view_limit")
+            .setCheck("Transition")
+            .appendField("getData(transition)");
+        this.setOutput(true, "Map<String,Field>");
+        this.setTooltip('Get data by Transition');
+        this.setColour(65)
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['getdata_transition_case'] = {
+    init: function() {
+        this.appendValueInput("view_limit")
+            .setCheck("Transition")
+            .appendField("getData(transition,");
+        this.appendValueInput("usecase")
+            .setCheck("Case")
+            .appendField("              usecase)");
+        this.setOutput(true, "Map<String,Field>");
+        this.setTooltip('Get data by Transition and Case');
+        this.setColour(65)
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['assigntask_control_case_user'] = {
+    init: function() {
+        this.appendValueInput("control")
+            .setCheck("String")
+            .appendField("assignRole(\"control\",");
+        this.appendValueInput("case")
+            .setCheck("Case")
+            .appendField("              eCase,");
+        this.appendValueInput("user")
+            .setCheck("User")
+            .appendField("              user)");
+        this.setOutput(true, "Task");
+        this.setTooltip('Assign task by control, case and user.');
+        this.setColour(65)
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['assigntask_task_user'] = {
+    init: function() {
+        this.appendValueInput("task")
+            .setCheck("Task")
+            .appendField("assignTask(task,");
+        this.appendValueInput("user")
+            .setCheck("User")
+            .appendField("              user)");
+        this.setOutput(true, "Task");
+        this.setColour(65);
+        this.setTooltip('Assign task by task and user');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+Blockly.Blocks['assigntask_tasks'] = {
+    init: function() {
+        this.appendValueInput("tasks")
+            .setCheck("List<Task>")
+            .appendField("assignTask(task)");
+        this.setOutput(true, "Task");
+        this.setColour(65);
+        this.setTooltip('Assign task by tasks');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['createcase_identifier_title_colour_author'] = {
+    init: function() {
+        this.appendValueInput("identifier")
+            .setCheck("String")
+            .appendField("createCase(identifier,");
+        this.appendValueInput("title")
+            .setCheck("String")
+            .appendField("                   title,");
+        this.appendValueInput("colour")
+            .setCheck("String")
+            .appendField("                   colour,");
+        this.appendValueInput("author")
+            .setCheck("User")
+            .appendField("                   user)");
+        this.setOutput(true, "Case");
+        this.setColour(65);
+        this.setTooltip('Create case by identifier, title, colour and author.');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['createcase_net_title_colour_author'] = {
+    init: function() {
+        this.appendValueInput("net")
+            .setCheck("PetriNet")
+            .appendField("createCase(net,");
+        this.appendValueInput("title")
+            .setCheck("String")
+            .appendField("                   title,");
+        this.appendValueInput("colour")
+            .setCheck("String")
+            .appendField("                   colour,");
+        this.appendValueInput("author")
+            .setCheck("User")
+            .appendField("                   user)");
+        this.setOutput(true, "Case");
+        this.setColour(65);
+        this.setTooltip('Create case by PetriNet, title, colour and author.');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['generate'] = {
+    init: function() {
+        this.appendValueInput("method")
+            .setCheck("String")
+            .appendField("generate(method,");
+        this.appendDummyInput()
+            .appendField("constant,")
+            .appendField(new Blockly.FieldDropdown([["always", "always"], ["once", "once"]]), "constant");
+        this.appendValueInput("field")
+            .setCheck("Field")
+            .appendField("field)");
+        this.setOutput(true, "Case");
+        this.setColour(65);
+        this.setTooltip('Generate');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['make'] = {
+    init: function() {
+        this.appendValueInput("field")
+            .setCheck("Field")
+            .appendField("make (field,");
+        this.appendDummyInput()
+            .appendField("behaviour,")
+            .appendField(new Blockly.FieldDropdown([["visible", "visible"], ["editable", "editable"], ["required", "required"], ["optional", "optional"], ["hidden", "hidden"], ["forbidden", "forbidden"]]), "behaviour");
+        this.appendValueInput("transition")
+            .setCheck("Transition")
+            .appendField("transition");
+        this.appendValueInput("condition")
+            .setCheck("Closure<Boolean>")
+            .appendField("when condition");
+        this.setOutput(true);
+        this.setColour(65);
+        this.setTooltip('Make');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['setdata_task_fields'] = {
+    init: function() {
+        this.appendValueInput("task")
+            .setCheck("Task")
+            .appendField("setData(task,");
+        this.appendValueInput("fields")
+            .setCheck("Map<String,{type,value}>")
+            .appendField("             fields)");
+        this.setOutput(true);
+        this.setColour(65);
+        this.setTooltip('Set data by task and fields');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+Blockly.Blocks['setdata_transition_fields'] = {
+    init: function() {
+        this.appendValueInput("transition")
+            .setCheck("Transition")
+            .appendField("setData(transition,");
+        this.appendValueInput("fields")
+            .setCheck("Map<String,{type,value}>")
+            .appendField("             fields)");
+        this.setOutput(true);
+        this.setColour(65);
+        this.setTooltip('Set data by transition and fields');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['setdata_transitionid_case_fields'] = {
+    init: function() {
+        this.appendValueInput("transition")
+            .setCheck("String")
+            .appendField("setData(transitionID,");
+        this.appendValueInput("case")
+            .setCheck("Case")
+            .appendField("             case,");
+        this.appendValueInput("fields")
+            .setCheck("Map<String,{type,value}>")
+            .appendField("             fields)");
+        this.setOutput(true);
+        this.setColour(65);
+        this.setTooltip('Set data by transitionId, case and fields');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['change_value'] = {
+    init: function() {
+        this.appendValueInput("field")
+            .setCheck("Field")
+            .appendField("change(field,");
+        this.appendValueInput("newValueClosure")
+            .setCheck("Closure")
+            .appendField("            newValueClosure)");
+        this.setOutput(true);
+        this.setColour(65);
+        this.setTooltip('Change value');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['change_value_about'] = {
+    init: function() {
+        this.appendValueInput("field")
+            .setCheck("Field")
+            .appendField("change(field,");
+        this.appendValueInput("newValueClosure")
+            .setCheck("Closure")
+            .appendField("            newValueClosure)");
+        this.setOutput(true);
+        this.setColour(65);
+        this.setTooltip('Change about');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['change_choices'] = {
+    init: function() {
+        this.appendValueInput("field")
+            .setCheck("Field")
+            .appendField("change field choice");
+        this.appendValueInput("newValueClosure")
+            .setCheck("Closure<List<String>|Set<String>>")
+            .appendField("            newValueClosure");
+        this.setOutput(true);
+        this.setColour(65);
+        this.setTooltip('Change choices');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['change_options'] = {
+    init: function() {
+        this.appendValueInput("field")
+            .setCheck("Field")
+            .appendField("change field options");
+        this.appendValueInput("newValueClosure")
+            .setCheck("Closure<Map<String, String|I18NString>>")
+            .appendField("            newValueClosure");
+        this.setOutput(true);
+        this.setColour(65);
+        this.setTooltip('Change options');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['change_case_property'] = {
+    init: function() {
+        this.appendValueInput("property")
+            .setCheck("String")
+            .appendField("changeCaseProperty");
+        this.appendValueInput("newValueClosure")
+            .setCheck("Field")
+            .appendField(" about  newValueClosure");
+        this.setOutput(true);
+        this.setColour(65);
+        this.setTooltip('Change case property.');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['findtask'] = {
+    init: function() {
+        this.appendValueInput("query")
+            .setCheck("QueryDSL Predicate")
+            .appendField("findTask");
+        this.setOutput(true, "Task");
+        this.setColour(65);
+        this.setTooltip('Find task by query.');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['findtasks'] = {
+    init: function() {
+        this.appendValueInput("query")
+            .setCheck("QueryDSL Predicate")
+            .appendField("findTasks");
+        this.setOutput(true, "List<Task>");
+        this.setColour(65);
+        this.setTooltip('Find tasks by query');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['findtasks_page'] = {
+    init: function() {
+        this.appendValueInput("query")
+            .setCheck("QueryDSL Predicate")
+            .appendField("findTasks (query,");
+        this.appendValueInput("page")
+            .setCheck("Pageable")
+            .appendField("                 page)");
+        this.setOutput(true, "List<Task>");
+        this.setColour(65);
+        this.setTooltip('Find tasks by query and page.');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['findcase'] = {
+    init: function() {
+        this.appendValueInput("query")
+            .setCheck("QueryDSL Predicate")
+            .appendField("findCase (query)");
+        this.setOutput(true, "Case");
+        this.setColour(65);
+        this.setTooltip('Find case by query.');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+Blockly.Blocks['findcases'] = {
+    init: function() {
+        this.appendValueInput("query")
+            .setCheck("QueryDSL Predicate")
+            .appendField("findCases (query)");
+        this.setOutput(true, "List<Case>");
+        this.setColour(65);
+        this.setTooltip('Find cases by query');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['findcases_page'] = {
+    init: function() {
+        this.appendValueInput("query")
+            .setCheck("QueryDSL Predicate")
+            .appendField("findCases (query,");
+        this.appendValueInput("page")
+            .setCheck("Pageable")
+            .appendField("                  page)");
+        this.setOutput(true, "List<Case>");
+        this.setColour(65);
+        this.setTooltip('Find cases by query and page.');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+Blockly.Blocks['execute'] = {
+    init: function() {
+        this.appendValueInput("transitionID")
+            .setCheck("String")
+            .appendField("execute transitionId,");
+        this.appendValueInput("query")
+            .setCheck("QueryDSL Predicate")
+            .appendField("where query");
+        this.appendValueInput("data")
+            .setCheck("Map<String,Object>")
+            .appendField("with data");
+        this.setOutput(true);
+        this.setColour(65);
+        this.setTooltip('Execute.');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
--- blockly/generators/java/petriflow.js
+++ blockly/generators/java/petriflow.js
@@ -9,3 +9,217 @@ Blockly.Java['block_assignrole'] = function(block) {
     var code = 'assignRole('+roleID+')';
     return code;
 };
+
+Blockly.Java['cancel_task_taskid'] = function(block) {
+    var value_taskid = Blockly.Java.valueToCode(block, 'taskId', Blockly.Java.ORDER_ATOMIC);
+    var code = 'cancelTask('+value_taskid+')';
+    return code;
+};
+
+Blockly.Java['cancel_task_task_case'] = function(block) {
+    var value_taskid = Blockly.Java.valueToCode(block, 'taskId', Blockly.Java.ORDER_ATOMIC);
+    var value_acase = Blockly.Java.valueToCode(block, 'aCase', Blockly.Java.ORDER_ATOMIC);
+    var code = 'cancelTask('+value_taskid+', '+value_acase+')';
+    return code;
+};
+
+Blockly.Java['cancel_task_task'] = function(block) {
+    var value_task = Blockly.Java.valueToCode(block, 'taskId', Blockly.Java.ORDER_ATOMIC);
+    var code = 'cancelTask('+value_task+')';
+    return code;
+};
+
+Blockly.Java['finish_task_taskid'] = function(block) {
+    var value_taskid = Blockly.Java.valueToCode(block, 'taskId', Blockly.Java.ORDER_ATOMIC);
+    var code = 'finishTask('+value_taskid+')';
+    return code;
+};
+
+Blockly.Java['finish_task_task_case'] = function(block) {
+    var value_taskid = Blockly.Java.valueToCode(block, 'taskId', Blockly.Java.ORDER_ATOMIC);
+    var value_acase = Blockly.Java.valueToCode(block, 'aCase', Blockly.Java.ORDER_ATOMIC);
+    var code = 'finishTask('+value_taskid+', '+value_acase+')';
+    return code;
+};
+
+Blockly.Java['finish_task_task'] = function(block) {
+    var value_task = Blockly.Java.valueToCode(block, 'taskId', Blockly.Java.ORDER_ATOMIC);
+    var code = 'finishTask('+value_task+')';
+    return code;
+};
+
+Blockly.Java['getdata_task'] = function(block) {
+    var value_task = Blockly.Java.valueToCode(block, 'task', Blockly.Java.ORDER_ATOMIC);
+    var code = 'getData('+value_task+')';
+    return code;
+};
+
+Blockly.Java['getdata_transition'] = function(block) {
+    var value_view_limit = Blockly.Java.valueToCode(block, 'view_limit', Blockly.Java.ORDER_ATOMIC);
+    var code = 'getData('+value_view_limit+')';
+    return code;
+};
+
+Blockly.Java['getdata_transition_case'] = function(block) {
+    var value_view_limit = Blockly.Java.valueToCode(block, 'view_limit', Blockly.Java.ORDER_ATOMIC);
+    var value_usecase = Blockly.Java.valueToCode(block, 'usecase', Blockly.Java.ORDER_ATOMIC);
+    var code = 'getData('+value_view_limit+', '+value_usecase+')';
+    return code;
+};
+
+Blockly.Java['assigntask_control_case_user'] = function(block) {
+    var value_control = Blockly.Java.valueToCode(block, 'control', Blockly.Java.ORDER_ATOMIC);
+    var value_case = Blockly.Java.valueToCode(block, 'case', Blockly.Java.ORDER_ATOMIC);
+    var value_user = Blockly.Java.valueToCode(block, 'user', Blockly.Java.ORDER_ATOMIC);
+    var code = 'assignTask('+value_control+', '+value_case+', '+value_user+')';
+    return code;
+};
+
+Blockly.Java['assigntask_task_user'] = function(block) {
+    var value_task = Blockly.Java.valueToCode(block, 'task', Blockly.Java.ORDER_ATOMIC);
+    var value_user = Blockly.Java.valueToCode(block, 'user', Blockly.Java.ORDER_ATOMIC);
+    var code = 'assignTask('+value_task+', '+value_user+')';
+    return code;
+};
+Blockly.Java['assigntask_tasks'] = function(block) {
+    var value_tasks = Blockly.Java.valueToCode(block, 'tasks', Blockly.Java.ORDER_ATOMIC);
+    var code = 'assignTask('+value_tasks+')';
+    return code;
+};
+Blockly.Java['createcase_identifier_title_colour_author'] = function(block) {
+    var value_identifier = Blockly.Java.valueToCode(block, 'identifier', Blockly.Java.ORDER_ATOMIC);
+    var value_title = Blockly.Java.valueToCode(block, 'title', Blockly.Java.ORDER_ATOMIC);
+    var value_colour = Blockly.Java.valueToCode(block, 'colour', Blockly.Java.ORDER_ATOMIC);
+    var value_author = Blockly.Java.valueToCode(block, 'author', Blockly.Java.ORDER_ATOMIC);
+    var code = 'createCase('+value_identifier+', '+value_title+', '+value_colour+', '+value_author+')';
+    return code;
+};
+
+Blockly.Java['createcase_net_title_colour_author'] = function(block) {
+    var value_net = Blockly.Java.valueToCode(block, 'net', Blockly.Java.ORDER_ATOMIC);
+    var value_title = Blockly.Java.valueToCode(block, 'title', Blockly.Java.ORDER_ATOMIC);
+    var value_colour = Blockly.Java.valueToCode(block, 'colour', Blockly.Java.ORDER_ATOMIC);
+    var value_author = Blockly.Java.valueToCode(block, 'author', Blockly.Java.ORDER_ATOMIC);
+    var code = 'createCase('+value_net+', '+value_title+', '+value_colour+', '+value_author+')';
+    return code;
+};
+
+Blockly.Java['generate'] = function(block) {
+    var value_method = Blockly.Java.valueToCode(block, 'method', Blockly.Java.ORDER_ATOMIC);
+    var dropdown_constant = block.getFieldValue('constant');
+    var value_field = Blockly.Java.valueToCode(block, 'field', Blockly.Java.ORDER_ATOMIC);
+    var code = 'generate "'+value_method+'", '+dropdown_constant+'into '+value_field;
+    return code;
+};
+Blockly.Java['make'] = function(block) {
+    var value_field = Blockly.Java.valueToCode(block, 'field', Blockly.Java.ORDER_ATOMIC);
+    var dropdown_behaviour = block.getFieldValue('behaviour');
+    var value_transition = Blockly.Java.valueToCode(block, 'transition', Blockly.Java.ORDER_ATOMIC);
+    var value_condition = Blockly.Java.valueToCode(block, 'condition', Blockly.Java.ORDER_ATOMIC);
+    var code = 'make '+value_field+' '+dropdown_behaviour+' on '+value_transition+' when '+value_condition;
+    return code;
+};
+
+Blockly.Java['setdata_task_fields'] = function(block) {
+    var value_task = Blockly.Java.valueToCode(block, 'task', Blockly.Java.ORDER_ATOMIC);
+    var value_fields = Blockly.Java.valueToCode(block, 'fields', Blockly.Java.ORDER_ATOMIC);
+    var code = 'setData('+value_task+', '+value_fields+')';
+    return code;
+};
+
+Blockly.Java['setdata_transition_fields'] = function(block) {
+    var value_transition = Blockly.Java.valueToCode(block, 'transition', Blockly.Java.ORDER_ATOMIC);
+    var value_fields = Blockly.Java.valueToCode(block, 'fields', Blockly.Java.ORDER_ATOMIC);
+    var code = 'setData('+value_transition+', '+value_fields+')';
+    return code;
+};
+
+Blockly.Java['setdata_transitionid_case_fields'] = function(block) {
+    var value_transition = Blockly.Java.valueToCode(block, 'transitionID', Blockly.Java.ORDER_ATOMIC);
+    var value_case = Blockly.Java.valueToCode(block, 'case', Blockly.Java.ORDER_ATOMIC);
+    var value_fields = Blockly.Java.valueToCode(block, 'fields', Blockly.Java.ORDER_ATOMIC);
+    var code = 'setData('+value_transition+', '+', '+value_case+value_fields+')';
+    return code;
+};
+
+Blockly.Java['change_value'] = function(block) {
+    var value_field = Blockly.Java.valueToCode(block, 'field', Blockly.Java.ORDER_ATOMIC);
+    var value_newvalueclosure = Blockly.Java.valueToCode(block, 'newValueClosure', Blockly.Java.ORDER_ATOMIC);
+    var code = 'change ' +value_field+' value '+value_newvalueclosure;
+    return code;
+};
+
+Blockly.Java['change_value_about'] = function(block) {
+    var value_field = Blockly.Java.valueToCode(block, 'field', Blockly.Java.ORDER_ATOMIC);
+    var value_newvalueclosure = Blockly.Java.valueToCode(block, 'newValueClosure', Blockly.Java.ORDER_ATOMIC);
+    var code = 'change ' +value_field+' about '+value_newvalueclosure;
+    return code;
+};
+
+Blockly.Java['change_choices'] = function(block) {
+    var value_field = Blockly.Java.valueToCode(block, 'field', Blockly.Java.ORDER_ATOMIC);
+    var value_newvalueclosure = Blockly.Java.valueToCode(block, 'newValueClosure', Blockly.Java.ORDER_ATOMIC);
+    var code = 'change ' +value_field+' choices '+value_newvalueclosure;
+    return code;
+};
+
+Blockly.Java['change_options'] = function(block) {
+    var value_field = Blockly.Java.valueToCode(block, 'field', Blockly.Java.ORDER_ATOMIC);
+    var value_newvalueclosure = Blockly.Java.valueToCode(block, 'newValueClosure', Blockly.Java.ORDER_ATOMIC);
+    var code = 'change ' +value_field+' options '+value_newvalueclosure;
+    return code;
+};
+
+Blockly.Java['change_case_property'] = function(block) {
+    var value_property = Blockly.Java.valueToCode(block, 'property', Blockly.Java.ORDER_ATOMIC);
+    var value_newvalueclosure = Blockly.Java.valueToCode(block, 'newValueClosure', Blockly.Java.ORDER_ATOMIC);
+    var code = 'changeCaseProperty'+value_property+' about '+value_newvalueclosure;
+    return code;
+};
+
+Blockly.Java['findtask'] = function(block) {
+    var value_query = Blockly.Java.valueToCode(block, 'query', Blockly.Java.ORDER_ATOMIC);
+    var code = 'findTask '+value_query;
+    return code;
+};
+
+Blockly.Java['findtasks'] = function(block) {
+    var value_query = Blockly.Java.valueToCode(block, 'query', Blockly.Java.ORDER_ATOMIC);
+    var code = 'findTasks('+value_query+')';
+    return code;
+};
+
+Blockly.Java['findtasks_page'] = function(block) {
+    var value_query = Blockly.Java.valueToCode(block, 'query', Blockly.Java.ORDER_ATOMIC);
+    var value_page = Blockly.Java.valueToCode(block, 'page', Blockly.Java.ORDER_ATOMIC);
+    var code = 'findTask('+value_query+', '+value_page+')';
+    return code;
+};
+
+Blockly.Java['findcase'] = function(block) {
+    var value_query = Blockly.Java.valueToCode(block, 'query', Blockly.Java.ORDER_ATOMIC);
+    var code = 'findCase('+value_query+')';
+    return code;
+};
+
+
+Blockly.Java['findcases'] = function(block) {
+    var value_query = Blockly.Java.valueToCode(block, 'query', Blockly.Java.ORDER_ATOMIC);
+    var code = 'findCases('+value_query+')';
+    return code;
+};
+
+Blockly.Java['findcases_page'] = function(block) {
+    var value_query = Blockly.Java.valueToCode(block, 'query', Blockly.Java.ORDER_ATOMIC);
+    var value_page = Blockly.Java.valueToCode(block, 'page', Blockly.Java.ORDER_ATOMIC);
+    var code = 'findCases('+value_query+', '+value_page+')';
+    return code;
+};
+
+Blockly.Java['execute'] = function(block) {
+    var value_transitionid = Blockly.Java.valueToCode(block, 'transitionID', Blockly.Java.ORDER_ATOMIC);
+    var value_query = Blockly.Java.valueToCode(block, 'query', Blockly.Java.ORDER_ATOMIC);
+    var value_data = Blockly.Java.valueToCode(block, 'data', Blockly.Java.ORDER_ATOMIC);
+    var code = 'execute '+value_transitionid+' where '+value_query+' with '+value_data;
+    return code;
+};
